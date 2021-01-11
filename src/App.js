b3af7fb2cd324d9bc98deffc8b5d60183c119325
Luis Chamosa <luis.chamosa@toptal.com>
2021-01-11 14:58:50 -0600

    final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function clickTop(){
+    var img = document.getElementById("imgLogo");   
+    var topNumbers = img.style.top.replace('px', '');
+    var top = parseInt(topNumbers, 10);
+    if (isNaN(top)){
+      img.style.top = '-10px';    
+    }
+    else{
+      var div = document.getElementById("divCenter").clientWidth;
+      var minLeft = ((div - img.clientWidth) / 2) * -1;
+      if(top >minLeft){
+        img.style.top = `${top - 10}px`
+      }
+    }   
+  }
+  function clickDown(){
+    var img = document.getElementById("imgLogo");   
+    var topNumbers = img.style.top.replace('px', '');
+    var top = parseInt(topNumbers, 10);
+    if (isNaN(top)){
+      img.style.top = '10px';    
+    }
+    else{
+      var div = document.getElementById("divCenter").clientWidth;
+      var maxLeft = (div - img.clientWidth) / 2;
+      if(top < maxLeft){
+        img.style.top = `${top + 10}px`
+      }
+    }
+  }
+  function clickLeft(){
+    var img = document.getElementById("imgLogo");   
+    var leftNumbers = img.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+    if (isNaN(left)){
+      img.style.left = '-10px';    
+    }
+    else{
+      var div = document.getElementById("divCenter").clientWidth;
+      var minLeft = ((div - img.clientWidth) / 2) * -1;
+      if(left >minLeft){
+        img.style.left = `${left - 10}px`;
+      }
+    }   
+  }
+  function clickRigth(){
+    var img = document.getElementById("imgLogo");   
+    var leftNumbers = img.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+    if (isNaN(left)){
+      img.style.left = '10px';    
+    }
+    else{
+      var div = document.getElementById("divCenter").clientWidth;
+      var maxLeft = (div - img.clientWidth) / 2;
+      if(left < maxLeft){       
+        img.style.left = `${left + 10}px`;
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-container">
+      <div className="div-corner"></div>
+      <div className="div-horizontal-button" onClick={clickTop}></div>
+      <div className="div-corner"></div>
+      <div className="div-vertical-button" onClick={clickLeft}></div>
+      <div id="divCenter" className="div-center">
+        <img id="imgLogo" class="image" src="./logo.png" alt="Digital on US" />
+      </div>
+      <div className="div-vertical-button" onClick={clickRigth}></div>
+      <div className="div-corner"></div>
+      <div className="div-horizontal-button" onClick={clickDown}></div>
+      <div className="div-corner"></div>
     </div>
   );
 }
-
 export default App;
